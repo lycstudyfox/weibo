4bbcd4b1b66c9c788f5fea1e61e3e27137eb039c
Lyc <lyc@studyfox.cn>
2019-02-07 09:09:49 +0000

    分离头部和底部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <a href="http://www.studyfox.cn/263.html" target="_blank">雪狐Laravel项目实战第一季微博系统</a>
+
+    <div class="float-right">
+        <a href="http://www.studyfox.cn">雪狐网</a>
+    </div>
+</footer>
