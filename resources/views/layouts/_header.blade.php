8581f66e699fc29473cc08087e86a8b2cc0255ee
Lyc <lyc@studyfox.cn>
2019-02-10 02:11:13 +0000

    改LOGO-测试

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a href="/" class="navbar-brand">雪狐网</a>
+        <a href="/" class="navbar-brand">雪狐微博</a>
         <ul class="navbar-nav justify-content-end">
             <li class="nav-item"><a href="{{ route('help') }}" class="nav-link">帮助</a></li>
             <li class="nav-item"><a href="javascript:;" class="nav-link">登录</a></li>
