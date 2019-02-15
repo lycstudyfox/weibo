136204d4709f601aa088925668722b6585da4b32
Lyc <lyc@studyfox.cn>
2019-02-15 09:30:57 +0000

    添加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,4 +10,14 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'emial' => 'required|email|unique:users|max:200',
+            'password' => 'required|confirmed|min:6',
+        ]);
+        return;
+    }
 }
