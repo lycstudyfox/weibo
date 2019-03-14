34ffc33447ea46cb9ce85b15a5d363ac5bb13f99
Lyc <lyc@studyfox.cn>
2019-03-14 06:28:42 +0000

    删除微博

--- resources/views/users/_status.blade.php
+++ resources/views/users/_status.blade.php
@@ -6,4 +6,12 @@
         <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy', $status)
+        <form method="post" action="{{ route('statuses.destroy', $status->id) }}">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+        </form>
+    @endcan
 </li>
